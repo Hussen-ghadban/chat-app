901490f141ed1a0c4107054958e7ec40b4a12477
Hessen <houssein.ghadban99@gmail.com>
2024-12-21 01:59:10 +0200

    project completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import HomePage from "./pages/HomePage";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
 import SettingsPage from "./pages/SettingsPage";
-import ProfilePage from "./pages/profilePage"; 
+import ProfilePage from "./pages/ProfilePage"; 
 
 import { Routes, Route, Navigate } from "react-router-dom";
 import { useAuthStore } from "./store/useAuthStore";
