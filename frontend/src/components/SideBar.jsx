0513eb66140cdaf26531ebd4d3c8de028d60bb62
Hessen <houssein.ghadban99@gmail.com>
2024-12-21 16:39:57 +0200

    change name of a file

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -4,7 +4,9 @@ import { useAuthStore } from "../store/useAuthStore";
 import SidebarSkeleton from "./skeletons/SidebarSkeleton";
 import { Users } from "lucide-react";
 
+
 const Sidebar = () => {
+  
   const { getUsers, users, selectedUser, setSelectedUser, isUsersLoading } = useChatStore();
 
   const { onlineUsers } = useAuthStore();
